e27f242735d3ed896c2114e6750beddea5464f0f
Giles Van Gruisen <gilesvangruisen@me.com>
2014-10-06 13:02:40 -0400

    ios 8 check for registerWithAPNS and checkIfRegisteredWithAPNS

--- inspector/ios-inspector/ForgeModule/push_API.m
+++ inspector/ios-inspector/ForgeModule/push_API.m
@@ -1,14 +1,32 @@
 #import "push_API.h"
 
+#define kNotificationSettings UIUserNotificationTypeSound | UIUserNotificationTypeBadge | UIUserNotificationTypeAlert
+#define kNotificationTypes UIRemoteNotificationTypeSound | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert
+
 @implementation push_API
 
-+ (void)checkIfRegisteredWithAPNS:(ForgeTask *)task
++ (void)checkIfRegisteredWithAPN:(ForgeTask *)task
 {
     [ForgeLog d:@"[FETCHNOTES] checkIfRegisteredWithAPNS"];
-    
-    if ([[UIApplication sharedApplication] enabledRemoteNotificationTypes] != 0) {
+
+    UIApplication *application = [UIApplication sharedApplication];
+
+    if ([application respondsToSelector:@selector(isRegisteredForRemoteNotifications)]) {
+
+        // iOS 8 check
+        if ([application isRegisteredForRemoteNotifications]) {
+            [task success:nil];
+        } else {
+            [task error:nil];
+        }
+
+    } else if ([application enabledRemoteNotificationTypes] != UIRemoteNotificationTypeNone) {
+
+        // iOS 7 check success
         [task success: nil];
     } else {
+
+        // iOS 7 check failure
         [task error: nil];
     }
 }
@@ -16,8 +34,26 @@
 + (void)registerWithAPNS:(ForgeTask *)task
 {
     [ForgeLog d:@"[FETCHNOTES] registerWithAPNS"];
-    
-    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeAlert|UIRemoteNotificationTypeSound];
+
+    UIApplication *application = [UIApplication sharedApplication];
+
+    // Check if application responds to iOS 8 only notification method
+    if ([application respondsToSelector:@selector(isRegisteredForRemoteNotifications)]) {
+
+        // iOS 8 way of doing things
+        [application registerForRemoteNotifications];
+
+        // We should be able to leave categories nil since we're not yet doing anything special
+        UIUserNotificationSettings *notificationSettings = [UIUserNotificationSettings settingsForTypes:kNotificationSettings categories:nil];
+        [application registerUserNotificationSettings:notificationSettings];
+
+    } else {
+
+        // iOS 7 way of doing things
+        [application registerForRemoteNotificationTypes:kNotificationTypes];
+
+    }
+
     [task success: nil];
 }
 
